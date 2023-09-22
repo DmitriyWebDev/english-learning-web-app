59ddd5f1d7195447e7dac178b9caa1ee328eb977
DmitriyWebDev <d-man_real_akkaunt.ru@mail.ru>
2023-09-22 23:43:01 +0600

    Fix type reexport error

--- src/entities/term/ui/index.ts
+++ src/entities/term/ui/index.ts
@@ -1,1 +1,2 @@
-export { TermForm, Props as TermFormProps } from './TermForm/TermForm';
+export { TermForm } from './TermForm/TermForm';
+export type { Props as TermFormProps } from './TermForm/TermForm';
